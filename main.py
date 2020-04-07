0ca8bd7ab886453962f8336b4e483c4ebc03e6a2
Jeremy Ward <jeremy@jmichaelward.com>
2020-04-07 14:55:35 -0500

    Move 404 handler back into main app file.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-from setup import app, db
+from flask import make_response, render_template
+from setup import app
 from src.routes import api, view
 
 app.register_blueprint(api.routes)
 app.register_blueprint(view.routes)
+
+
+@app.errorhandler(404)
+def show_404(message):
+    return make_response(render_template('404.html', message=message), 404)
 
 
 if __name__ == '__main__':
