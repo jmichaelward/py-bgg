1987317c2fea2aeb06986f9b2484bf5f94dd728e
Jeremy Ward <jeremy@jmichaelward.com>
2020-04-07 14:55:48 -0500

    Remove direct module call for now.

--- src/routes/view.py
+++ src/routes/view.py
@@ -50,7 +50,3 @@ def index():
     The main page is just going to show links to the various API routes.
     """
     return render_template('main.html')
-
-
-if __name__ == '__main__':
-    app.run()
