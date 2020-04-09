26962f16cf7b8d6526019143fe85a193cef44362
Jeremy Ward <jeremy@jmichaelward.com>
2020-04-09 09:21:01 -0500

    Create Config object and move secret key into environment variable.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from flask import Flask
+from config import Config
 from src.db import Database
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app.config.from_object(Config)
 
 db = Database()
 db.connect()
