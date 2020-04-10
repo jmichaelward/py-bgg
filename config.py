4c5669533aac695f9fe794a413a1c2f6376891e6
Jeremy Ward <jeremy@jmichaelward.com>
2020-04-10 09:39:47 -0500

    Create additional environment variables and update config.

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
